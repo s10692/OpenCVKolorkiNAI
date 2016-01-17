d8ae08d8ed08e85d53ae4cc1cee63bc44ba15bbb
Dariusz Felskowski <s10692@pjwstk.edu.pl>
2016-01-17 18:03:45 +0100

    OpenCVKolorkiNAI

--- OpenCVKolorkiNAI/OpenCVKolorkiNAI/OpenCVKolordkiNAI.cpp
+++ OpenCVKolorkiNAI/OpenCVKolorkiNAI/OpenCVKolordkiNAI.cpp
@@ -15,3 +15,64 @@ int Nasycenie_MIN = 0;
 int Nasycenie_MAX = 256;
 int Wartosc_MIN = 0;
 int Wartosc_MAX = 256;
+
+
+const int Wysokosc_ramy = 480;
+const int Szerokosc_ramy = 640;
+
+const int MAX_wykryte_obiekty = 50;
+
+const int MIN_powierzchnia_obiektu = 15 * 15;
+const int MAX_powierzchnia_obiektu = Wysokosc_ramy * Szerokosc_ramy / 2;
+
+
+const string okno = "Oryginalny Obraz";
+const string okno1 = "Obraz HSV";
+const string okno2 = "Obraz programowalny";
+const string okno3 = "Obraz po przekszta³ceniach";
+const string suwakiHSV = "suwakiHSV";
+
+bool Ruszenie_myszy;
+bool Tryb_kalibracji;
+bool Przeciaganie_myszy;
+
+bool Prostokat;
+
+cv::Point Poczatkowe_klikniecie, Obecna_pozycja_myszy;
+cv::Rect prostokatROI;
+vector<int> Odcien_ROI, Nasycenie_ROI, Wartosc_ROI;
+
+void przesuwanie(int, void*)
+{
+	//Bêdzie uzupe³niane
+}
+
+
+void tworzenieSuwakowHSV() {
+
+	namedWindow(suwakiHSV, 0);
+
+	char Nazwa_suwakow[50];
+
+	sprintf(Nazwa_suwakow, "Odcien_MIN", Odcien_MIN);
+	sprintf(Nazwa_suwakow, "Odcien_MAX", Odcien_MAX);
+	sprintf(Nazwa_suwakow, "Nasycenie_MIN", Nasycenie_MIN);
+	sprintf(Nazwa_suwakow, "Nasycenie_MAX", Nasycenie_MIN);
+	sprintf(Nazwa_suwakow, "Wartosc_MIN", Wartosc_MIN);
+	sprintf(Nazwa_suwakow, "wartosc_MAX", Wartosc_MIN);
+
+
+
+	createTrackbar("Odcien_MIN", suwakiHSV, &Odcien_MIN, 255, przesuwanie);
+	createTrackbar("Odcien_MAX", suwakiHSV, &Odcien_MIN, 255, przesuwanie);
+	createTrackbar("Nasycenie_MIN", suwakiHSV, &Nasycenie_MIN, 255, przesuwanie);
+	createTrackbar("Nasycenie_MIN", suwakiHSV, &Nasycenie_MIN, 255, przesuwanie);
+	createTrackbar("Wartosc_MIN", suwakiHSV, &Wartosc_MIN, 255, przesuwanie);
+	createTrackbar("Wartosc_MIN", suwakiHSV, &Wartosc_MIN, 255, przesuwanie);
+
+
+
+
+
+	
+}
