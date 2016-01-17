3363eb1c9e38f82f334337bc6c1741a360a1894c
Dariusz Felskowski <s10692@pjwstk.edu.pl>
2016-01-17 17:02:48 +0100

    Kolorki

--- OpenCVKolorkiNAI/OpenCVKolorkiNAI/OpenCVKolordkiNAI.cpp
+++ OpenCVKolorkiNAI/OpenCVKolorkiNAI/OpenCVKolordkiNAI.cpp
@@ -6,3 +6,12 @@
 #include <opencv2/highgui/highgui.hpp>
 
 
+using namespace cv;
+using namespace std;
+
+int Odcien_MIN = 0;
+int Odcien_MAX = 256;
+int Nasycenie_MIN = 0;
+int Nasycenie_MAX = 256;
+int Wartosc_MIN = 0;
+int Wartosc_MAX = 256;
